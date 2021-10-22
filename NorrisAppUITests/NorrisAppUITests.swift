c401172a4cb775dc690493cf98400e0efd53f29a
Lucas Moraes <lucas.moraes@westwing.com.br>
2021-10-22 10:23:50 -0300

    adding UI Testing / Cleaning project

--- NorrisAppUITests/NorrisAppUITests.swift
+++ NorrisAppUITests/NorrisAppUITests.swift
@@ -9,34 +9,50 @@ import XCTest
 
 class NorrisAppUITests: XCTestCase {
 
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
+        
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        /** launchArguments for offline testing with mocked data */
+//        app.launchArguments = ["-isStagingOfflineRunning"]
+        app.launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testViewDetail() {
+        
+        let animalCell = app.tables/*@START_MENU_TOKEN@*/.staticTexts["animal"]/*[[".cells.staticTexts[\"animal\"]",".staticTexts[\"animal\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        XCTAssert(animalCell.waitForExistence(timeout: 4))
+        XCTAssertTrue(animalCell.exists)
+        
+        animalCell.tap()
+        
+        let detailTitle = app.navigationBars["animal"]
+        
+        XCTAssert(detailTitle.waitForExistence(timeout: 4))
+        XCTAssertTrue(detailTitle.exists)
     }
+    
+    func testViewDetailAndGoBack() {
+        
+        let animalCell = app.tables.staticTexts["fashion"]
+        XCTAssert(animalCell.waitForExistence(timeout: 4))
+        animalCell.tap()
+        
+        let detailTitle = app.navigationBars["fashion"]
+        XCTAssert(detailTitle.waitForExistence(timeout: 4))
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        
+        let navBarButton = app.navigationBars["fashion"].buttons["Categories"]
+        navBarButton.tap()
+        
+        XCTAssert(app.navigationBars["Categories"].exists)
     }
 }
